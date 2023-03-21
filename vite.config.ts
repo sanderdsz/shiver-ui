fd250f43a3745c6acc7356e711aa95694aecf2fd
sander <sanderdsz@gmail.com>
2023-03-20 21:11:07 -0300

    chore: npm package structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as packageJson from './package.json'
+import dtsPlugin from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dtsPlugin({
+      insertTypesEntry: true
+    })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      name: 'Shiver UI',
+      formats: ['es', 'umd'],
+      fileName: (format) => `shiver-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.dependencies)],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'shiver-design-system': 'Shiver'
+        }
+      }
+    }
+  }
 })
